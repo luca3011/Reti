11c81923cbbbd869190d8c4c442a051c1b376663
gullpet <gullpet99@hotmail.it>
2020-10-07 15:15:44 +0200

    aggiunto consumatore.c

--- es0/C/Consumatore.c
+++ es0/C/Consumatore.c
@@ -1,28 +1,54 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#define MAX_STRING_LENGTH 256
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]){
-    char *file_in, read_char, buf[MAX_STRING_LENGTH]; int nread, fd;
+    char *file_in, read_char, *prefixstring; int nread, fd, sNumber, sLength;
     
     // controllo argomenti
-    if (argc != 2) { perror(" numero di argomenti sbagliato"); exit(1);}
-    file_in = argv[1];
-    
+    if (argc != 3 && argc != 2) { perror(" numero di argomenti sbagliato"); exit(1);}
+
+    if(argc == 3)	//se non c'è ridirezione dell'input
+    	file_in = argv[2];
+
+    sLength = strlen(argv[1]); //lunghezza della sequenza
+    char temp[sLength+1];
+    prefixstring = argv[1];
+
     // ciclo del programma
-    fd = open(file_in, O_RDONLY);
+    if(argc == 3)
+    	fd = open(file_in, O_RDONLY);
+    else	//ridirezione dell'input
+    	fd = 0;
+
     if (fd<0){ perror("P0: Impossibile aprire il file."); exit(2); }
     
+    sNumber=0;	//numero di sequenza progressivo
+
     while(nread = read(fd, &read_char, sizeof(char)))
     /* un carattere alla volta fino ad EOF*/
     {
-        if (nread > 0) 
-            putchar(read_char);
+        if (nread > 0){
+        	if(read_char == prefixstring[sNumber]){
+        		temp[sNumber++] = read_char;
+
+        		if(sNumber == sLength){
+        			sNumber = 0;
+        		}
+        	}
+        	else{
+        		temp[sNumber++] = read_char;
+        		temp[sNumber] = '\0';
+        		printf("%s", temp);
+        		sNumber = 0;
+        	}
+    	}
         else{
             printf("(PID %d) impossibile leggere dal file %s", getpid(), file_in);
             perror("Errore!"); close(fd); exit(3);
-            }
+        }
     }
     close(fd);
 }
