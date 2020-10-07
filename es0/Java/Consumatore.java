7a2ff89a4d7b5a6316c84e30c04ee1cc3e1379a4
luca3011 <lucatacinelli@gmail.com>
2020-10-07 18:11:50 +0200

    sistemato (finalmente) consumatore java

--- es0/Java/Consumatore.java
+++ es0/Java/Consumatore.java
@@ -1,30 +1,24 @@
 
+import static java.lang.Integer.parseInt;
+
 import java.io.*;
-public class Consumatore {
-    
-    public static void main(String[] args)
-    {
-        FileReader r = null;
+
+public class Consumatore2 {
+
+    public static void main(String[] args) {
+        Reader r = null;
         // fare controllo argomenti
-        
+
         String stringa = args[0];
-            String appoggio = "";
-            int flag = 0;
-            char nextChar = stringa.charAt(0);
-            int l = stringa.length();
 
-
-        if(args.length ==1)
-        {
+        if (args.length == 1) {
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
             int res;
             String inputl = "";
-            
 
-            try
-            {
+            try {
                 System.out.println("Quante righe vuoi inserire?");
-                res = Integer.parseInt(in.readLine());
+                res = parseInt(in.readLine());
                 for (int i =0; i<res; i++)
                 { 
                     System.out.println("Inserisci la nuova riga");
@@ -34,82 +28,21 @@ public class Consumatore {
             catch (NumberFormatException nfe) {nfe.printStackTrace(); System.exit(1);}
             catch (IOException e) {e.printStackTrace(); System.exit(2);}
 
-            StringReader out = new StringReader(inputl);
+            r = new StringReader(inputl);
 
-            try { 
-                int x; 
-                char ch;
-                while ((x = out.read()) >=0){   
-                    ch = (char) x;
-    
-                    //filtro stringa
-                    if(ch==nextChar)
-                    {
-                        do
-                        {   
-                            ch=(char) x;
-                            flag++;
-                            appoggio = appoggio+ch;
-                            nextChar = stringa.charAt(flag);
-                        }while(((x=out.read())>=0) && ((char)x==nextChar) && (flag < l-1) );
-                        
-                        if(flag < l-1)
-                            System.out.print(appoggio);
-                    
-                        appoggio="";
-                        flag=0;
-                        nextChar = stringa.charAt(0);
-                    } 
-                    else
-                    {
-                        System.out.print(ch);
-                    }
-                }
-                out.close();
-                }
-            catch(IOException ex)
-                {System.out.println("Errore di input"); System.exit(2);}
-
+            LettoreScrittore(r,stringa);
         }
         else if(args.length ==2)
         {
-            try { r = new FileReader(args[1]); }
+            try 
+            { 
+                r = new FileReader((args[1])); 
+            }
             catch(FileNotFoundException e)
             { System.out.println("File non trovato"); System.exit(1);}
             
-            try { 
-                int x; 
-                char ch;
-                while ((x = r.read()) >=0){   
-                    ch = (char) x;
-    
-                    //filtro stringa
-                    if(ch==nextChar)
-                    {
-                        do
-                        {   
-                            ch=(char) x;
-                            flag++;
-                            appoggio = appoggio+ch;
-                            nextChar = stringa.charAt(flag);
-                        }while(((x=r.read())>=0) && ((char)x==nextChar) && (flag < l-1) );
-                        
-                        if(flag < l-1)
-                            System.out.print(appoggio);
-                    
-                        appoggio="";
-                        flag=0;
-                        nextChar = stringa.charAt(0);
-                    } 
-                    else
-                    {
-                        System.out.print(ch);
-                    }
-                }
-                r.close();
-                }
-            catch(IOException ex)
-                {System.out.println("Errore di input"); System.exit(2);}
+            LettoreScrittore(r,stringa);
+            
         }
         else
         {
@@ -119,5 +52,44 @@ public class Consumatore {
 
     }
 
+   static  void LettoreScrittore(Reader r,String stringa)
+    {
+        int l = stringa.length();
+        String appoggio="";
 
+        try { 
+            int snumber=0;
+            char ch;
+            int x;
+
+           while ((x = r.read()) >=0)
+            {
+                ch = (char) x;
+
+                if(ch == stringa.charAt(snumber))
+                {        
+                    appoggio += ch;
+                    snumber++;
+                    if(appoggio.length()==l)
+                    {
+                        appoggio="";
+                        snumber=0;
+                    }
+                }
+                else
+                {   
+                    appoggio += ch;
+                    snumber++;
+                    //appoggio[snumber] = '\0';
+                    System.out.print(appoggio);
+                    appoggio="";
+                    snumber=0;
+                }
+            }
+            r.close();
+            }
+        catch(IOException ex)
+            {System.out.println("Errore di input"); System.exit(2);}
+    }
+  
 }
