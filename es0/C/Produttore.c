d5e559674d9c8f36af4f67734709e975856908c6
RedDuality <giacomo_romanini2000@libero.it>
2020-10-07 15:19:32 +0200

    Update Produttore.c

--- es0/C/Produttore.c
+++ es0/C/Produttore.c
@@ -2,34 +2,45 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #define MAX_STRING_LENGTH 256
+int main(int argc, char *argv[])
+{
+    // preparazione programma
+    int fd, readValues, bytes_to_write, written;
+    char *file_out;
+    char buf[MAX_STRING_LENGTH], ch;
+    int righe, i;
+    char riga[MAX_STRING_LENGTH];
 
-int main(int argc, char* argv[]){ // preparazione programma
-    int fd, readValues, bytes_to_write, written; char *file_out;
-    char buf[MAX_STRING_LENGTH]; int righe, i; char riga[MAX_STRING_LENGTH];
-   
     // fare controllo argomenti
-    if (argc != 2) { perror(" numero di argomenti sbagliato …"); exit(EXIT_FAILURE);}
-   
-    // lettura #righe
-    file_out = argv[1];
-    
-    fd = open(file_out, O_WRONLY|O_CREAT|O_TRUNC, 00640);
-    if (fd < 0){ 
-        perror("P0: Impossibile creare/aprire il file"); exit(EXIT_FAILURE);
+    if (argc != 2)
+    {
+        perror(" numero di argomenti sbagliato …");
+        exit(EXIT_FAILURE);
     }
-    for(i=0; i<righe; ++i){
-        printf("Inserisci la nuova riga\n");
-        gets (riga);
-        /* la gets legge tutta la riga, separatori inclusi, e trasforma il fine
-        linea in fine stringa */
-        
-        // aggiungo il fine linea
-        riga[strlen(riga)+1]='\0';
-        riga[strlen(riga)]='\n';
-        written = write(fd, riga, strlen(riga)); // uso della primitiva
-        if (written <= 0){ 
-            perror("P0: errore nella scrittura sul file"); exit(2);
+
+    // lettura righe
+    file_out = argv[1];
+
+    fd = open(file_out, O_WRONLY | O_CREAT | O_TRUNC, 00640);
+
+    if (fd < 0)
+    {
+        perror("P0: Impossibile creare/aprire il file");
+        exit(EXIT_FAILURE);
+    }
+
+    // corpo produttore
+    
+    while (read(0,&ch,1))
+    {
+        written = write(fd, &ch, sizeof(ch)); // uso della primitiva
+
+        if (written <= 0)
+        {
+            perror("P0: errore nella scrittura sul file");
+            exit(EXIT_FAILURE);
         }
     }
     close(fd);
