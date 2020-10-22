37020c9ca1a2bb15a822022afb9ec77476657dc9
gullpet <gullpet99@hotmail.it>
2020-10-22 14:10:17 +0200

    modifiche finali

--- es2/da_fare/FileUtility.java
+++ es2/da_fare/FileUtility.java
@@ -9,18 +9,19 @@ public class FileUtility {
     
     static protected void trasferisci_a_byte_file_binario(DataInputStream src, DataOutputStream dest, long length)
             throws IOException {
-        // ciclo di lettura da sorgente e scrittura su destinazione
+
         byte[] buffer = new byte[BUFFER_SIZE];
-        int dyn_buffer_size = length < BUFFER_SIZE? (int)length : BUFFER_SIZE;    //dimensione del buffer
-        try { // esco dal ciclo alla lettura di un valore negativo-> EOF oppure in seguito alla lettura di length byte
+        int dyn_buffer_size = length < BUFFER_SIZE ? (int) length : BUFFER_SIZE; //dimensione del buffer
+        try {
             long nletti = 0; // numero di byte letti
+            // esco dal ciclo alla lettura di un valore negativo-> EOF oppure in seguito alla lettura di length byte
             while (nletti < length && src.read(buffer, 0, dyn_buffer_size) >= 0) {
                 dest.write(buffer, 0, dyn_buffer_size);
                 nletti += dyn_buffer_size;
                 if (length - nletti < BUFFER_SIZE)
                     dyn_buffer_size = (int) (length - nletti);
             }
-            
+
             dest.flush();
         } catch (EOFException e) {
             System.out.println("Problemi:");
