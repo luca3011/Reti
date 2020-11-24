ab9850f0e674cb6d55382fa5f068d3df7dd4e2ae
Luca <lucatacinelli@gmail.com>
2020-11-24 19:37:33 +0100

    finito cercatag

--- es7/da_fare/RegistryRemotoTagImpl.java
+++ es7/da_fare/RegistryRemotoTagImpl.java
@@ -3,6 +3,7 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Arrays;
 
+@SuppressWarnings("serial")
 public class RegistryRemotoTagImpl extends RegistryRemotoImpl implements RegistryRemotoTagServer {
 
 	final int tableSize = 100; //Tabella: le colonne contengono i 5 tag
@@ -22,11 +23,34 @@ public class RegistryRemotoTagImpl extends RegistryRemotoImpl implements Registr
 		 }
 	}
 
-	@Override
+	
 	public Remote[] cercaTag(String tag) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        int cont=0;
+        int tagindex=0;
+        Boolean found=false;
+        for(int i=0;i<tagNumber&&found==false;i++) {
+            if(tag.equals(nomi_tag[i])) {
+                tagindex=i;
+                found=true;
+            }
+        }
+        for(int i=0;i<tableSize;i++) {
+            if(tag_table[i][tagindex]==true) {
+                cont++;
+            }
+        }
+
+        Remote[] risultato = new Remote[cont];
+        cont=0;
+        for(int i=0;i<tableSize;i++) {
+            if(tag_table[i][tagindex]==true) {
+                risultato[cont++]=(Remote) table[i][1];
+            }
+        }
+
+
+        return risultato;
+    }
 
 	@Override
 	public int associaTag(String nome_logico_server, String tag) throws RemoteException {
