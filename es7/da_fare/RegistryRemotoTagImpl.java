ec041accc8e33c8b9ce0f9711c10bd5c72757a05
Luca <lucatacinelli@gmail.com>
2020-11-24 19:07:37 +0100

    Creato costruttore tagimpl

--- es7/da_fare/RegistryRemotoTagImpl.java
+++ es7/da_fare/RegistryRemotoTagImpl.java
@@ -1,10 +1,27 @@
-import java.rmi.Naming;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class RegistryRemotoTagImpl extends RegistryRemotoImpl implements RegistryRemotoTagServer {
 
+	public RegistryRemotoTagImpl() throws RemoteException {
+		super();
+	}
+
+	@Override
+	public Remote[] cercaTag(String tag) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int associaTag(String nome_logico_server, String tag) throws RemoteException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
 	
 
 }
