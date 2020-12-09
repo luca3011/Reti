d4b361c8038e7183475eafcd1b9e421a5e721cbb
gullpet <gullpet99@hotmail.it>
2020-12-09 10:23:51 -0500

    programma funzionante, da controllare/sistemare

--- es8/da_fare/client.c
+++ es8/da_fare/client.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <rpc/rpc.h>
+#include <ctype.h> //toupper()
 #include "file.h"
 
 #define DIM 100
@@ -7,7 +8,7 @@
 int main(int argc, char *argv[]){
     CLIENT *cl;
     Input dir;
-    Output file_result;
+    Output *file_result;
     int *dir_num;
     char *server;
     char *msg;
@@ -22,12 +23,13 @@ int main(int argc, char *argv[]){
         clnt_pcreateerror(server);
         exit(1);
     }
+
     msg = (char*)malloc(DIM);
     printf("Scansione: 'F' per conta in un file e 'D' per direttorio, EOF per terminare\n");
     //printf("Messaggio (max 100 caratteri)? "); 
     /* lettura della stringa da inviare o fine file */
     while(gets(msg)){
-        if(msg=="F"){
+        if(toupper(msg[0]) == 'F'){
             printf("Nome del file: (max 100 caratteri)? ");
             gets(msg);
             file_result = file_scan_1(&msg, cl);
@@ -36,39 +38,46 @@ int main(int argc, char *argv[]){
                 clnt_perror(cl, server);
                 exit(1);
             }
-            if (*file_result == NULL){ /* controllo errore risultato*/
-                fprintf(stderr, "%s: %s risultato non valido (NULL)\n",   argv[0], server);
+            if (file_result->caratteri == -1){ /* controllo errore risultato*/
+                fprintf(stderr, "%s: %s risultato non valido (-1,-1,-1)\n",   argv[0], server);
                 clnt_perror(cl, server);
                 exit(1);
             }
-            printf("Nel file %s sono stati trovati %d caratteri, %d parole e %d righe\n", msg, file_result.caratteri,file_result.parole,file_result.linee);
+            printf("Nel file %s sono stati trovati %d caratteri, %d parole e %d righe\n", msg, file_result->caratteri,file_result->parole,file_result->righe);
 
         }
-        else if(msg=="D"){
+        else if(toupper(msg[0])=='D'){
 
             printf("Nome del direttorio: (max 100 caratteri)? \n");
-            gets(dir.nome);
-            printf("Dimensione minima del file(bytes):\n");
-            scanf("%d",&dir.soglia);
-            dir_num = dir_scan_1(&msg, cl);
+            char buff[100];
+            gets(buff);
+            dir.nome = buff;
+            printf("Dimensione minima del file(bytes): (nomedir: %s)\n", dir.nome);
+            scanf("%d",&(dir.soglia));
+            dir_num = dir_scan_1(&dir, cl);
             if(dir_num == NULL){ /* controllo errore RPC */
                 fprintf(stderr, "%s: %s fallisce la rpc\n", argv[0], server);
                 clnt_perror(cl, server);
                 exit(1);
             }
-            if (*dir_num == NULL){ /* controllo errore risultato*/
-                fprintf(stderr, "%s: %s risultato non valido (NULL)\n",   argv[0], server);
+            if (*dir_num == -1){ /* controllo errore risultato*/
+                fprintf(stderr, "%s: %s risultato non valido (-1)\n",   argv[0], server);
                   clnt_perror(cl, server);
                   exit(1);
             }
-            printf("Nella directory %s ci sono %d files più grandi di %d bites",dir.nome,dir_num,dir.soglia);
+            printf("Nella directory %s ci sono %d files più grandi di %d bytes\n",dir.nome, *dir_num, dir.soglia);
         }
+        else{
+            continue;
+        }
+        
         printf("Scansione: 'F' per conta in un file e 'D' per direttorio, EOF per terminare\n");
-    }// while 
+    }
+
     gets(msg);
     free(msg);
     clnt_destroy(cl);
-    // Libero risorse: malloc e gestore di trasporto
+
     printf("Termino...\n");  
     exit(0); 
-} // main
+}
